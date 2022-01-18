9ed79467cc871b9da1ef77d603788cb51c143ea8
Shannon Pileggi <pileggis@mskcc.org>
2022-01-18 15:58:48 -0500

    count_drop example

---
+++ R/count_drop.R
@@ -0,0 +1,6 @@
+dat <- tibble(fruit = c("apple", "apple", "apple", "orange")) %>% 
+  mutate(fruit = as_factor(fruit) %>% fct_expand("apple", "orange", "banana"))
+
+dat %>% count(fruit)
+
+dat %>% count(fruit, .drop = FALSE)
