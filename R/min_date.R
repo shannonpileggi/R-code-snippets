ccd9611b92d79627780e5209054e212e1133d7e5
Shannon Pileggi <pileggis@mskcc.org>
2022-03-25 16:11:15 -0400

    more tests

--- R/min_date.R
+++ R/min_date.R
@@ -19,7 +19,9 @@ dat_original %>%
   rowwise() %>% 
   mutate(
     min_date = min(date_1, date_2, na.rm = TRUE)
-  ) 
+  ) -> check1
+
+writexl::write_xlsx(check1, "check1.xlsx")
 
 
 # warning reprex -------------------------------------------------------------------------------------
@@ -44,3 +46,38 @@ suppressWarnings({
     ))
 })
 
+
+suppressWarnings({
+dat %>%
+  rowwise() %>%
+  mutate(min_date =  min(date_1, date_2, na.rm = TRUE))
+})
+
+dat %>%
+  rowwise() %>%
+  suppressWarnings({
+  mutate(min_date =  min(date_1, date_2, na.rm = TRUE))
+  })  
+
+
+# weird excel export -----------------------------------------------------------
+
+
+library(tidyverse)
+library(lubridate, warn.conflicts = FALSE)
+
+# mixed POSIXct and Date fields ----
+dat_original <- tibble::tibble(
+  date_1 = c(NA_Date_, as_date("2022-01-01"), as_date("2022-01-01")),
+  date_2 = c(NA_Date_, NA_Date_, as_date("2021-01-01"))
+) 
+
+check1 <- dat_original %>% 
+  rowwise() %>% 
+  mutate(
+    min_date = min(date_1, date_2, na.rm = TRUE)
+  ) 
+
+
+writexl::write_xlsx(check1, "check1.xlsx")
+openxlsx::write.xlsx(check1, "check2.xlsx")
